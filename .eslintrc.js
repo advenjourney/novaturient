fab838c302252b3127c21e67a90d8aff97666577
Lukas Hirt <lukashirt@mail.com>
2021-01-12 17:54:52 +0100

    Init Vue

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  },
+
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ],
+
+  globals: {
+    TNS_APP_MODE: true,
+    TNS_APP_PLATFORM: true
+  }
+};
